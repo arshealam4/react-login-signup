a8b3499225c640bc72a3554a8e1ae00ac7aca237
Alam <arshe.alam@avrioc.com>
2021-06-27 17:18:23 +0400

    build folder added

---
+++ build/precache-manifest.4af3b1dec73565291e6fb723bbbc2424.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c3df8a47d05a8cc3beb",
+    "url": "/static/js/main.5c3df8a4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "5f2f0b7707e623872ce8",
+    "url": "/static/css/2.2f9761ed.chunk.css"
+  },
+  {
+    "revision": "5f2f0b7707e623872ce8",
+    "url": "/static/js/2.5f2f0b77.chunk.js"
+  },
+  {
+    "revision": "7cfcb123d501959ee5e4f34649d6b46f",
+    "url": "/index.html"
+  }
+];
